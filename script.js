a4de23cbb0e93137b181c5911f4ab663833eb75d
prabav01 <hprabav@gmail.com>
2026-02-08 12:58:40 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function gotoeasy(){
+    window.location.href="easy.html";
+}
+function gotomedium(){
+    window.location.href="medium.html";
+}
+function gotohard(){
+    window.location.href="hard.html";
+}
+function gotoextrahard(){
+    window.location.href="extrahard.html";
+}
